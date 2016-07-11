5d93da431638e30a57b44ce28c2875f76779d35d
Albert Yang <albertyang_mail@yeah.net>
2016-07-12 00:18:58 +0800

    summary

--- src/seqlist/SeqListTest.java
+++ src/seqlist/SeqListTest.java
@@ -12,8 +12,6 @@ public class SeqListTest {
 		seqList.insert(0);
 		seqList.display();
 		
-		//哈哈
-		
 		System.out.println(seqList.binarySearch(3) + "");
 		
 //		System.out.println(seqList.search(3));
